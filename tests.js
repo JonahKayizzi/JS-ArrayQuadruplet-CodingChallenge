fd69f83ceee6f6f1391da2cca4905f6e35cab197
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2023-05-22 19:27:28 +0300

    Modify testing assertion

--- tests.js
+++ tests.js
@@ -4,10 +4,10 @@ const assert = require('assert');
 // eslint-disable-next-line no-undef
 describe('return first array of numbers in arr summing up to s', function () {
   // eslint-disable-next-line no-undef
-  it('it should return []', function () {
+  it('it should return [0, 4, 7, 9]', function () {
     const givenArray = [2, 7, 4, 0, 9, 5, 1, 3];
     const sum = 20;
     const result = [0, 4, 7, 9];
-    assert.equal(findArrayQuadruplet(givenArray, sum), result);
+    assert.deepStrictEqual(findArrayQuadruplet(givenArray, sum), result);
   });
 });
